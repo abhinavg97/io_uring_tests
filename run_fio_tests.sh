3ee2e4e0d485d683cfd46256a607ad24f943ed86
Abhinav <gabhinav001@gmail.com>
2024-06-01 01:49:37 +0000

    update configs

--- run_fio_tests.sh
+++ run_fio_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RESULTS=results
+
 # Ensure the script runs with root privileges
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
@@ -13,23 +15,23 @@ iodepths=(1 2 4 8 16 32 64 128)
 for depth in "${iodepths[@]}"; do
     # Modify iou.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" iou.fio
-    sudo fio iou.fio --output=output_iou_${depth}_2core.txt
+    sudo fio iou.fio --output-format=json -o $RESULTS/output_iou_${depth}_1core.txt
     
     # Modify iou_p.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" iou_p.fio
-    sudo fio iou_p.fio --output=output_iou_p_${depth}_2core.txt
+    sudo fio iou_p.fio --output-format=json -o $RESULTS/output_iou_p_${depth}_1core.txt
     
     # Modify iou_k.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" iou_k.fio
-    sudo fio iou_k.fio --output=output_iou_k_${depth}_2core.txt
+    sudo fio iou_k.fio --output-format=json -o $RESULTS/output_iou_k_${depth}_1core.txt
 
     # Modify aio.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" aio.fio
-    sudo fio aio.fio --output=output_aio_${depth}_2core.txt
+    sudo fio aio.fio --output-format=json -o $RESULTS/output_aio_${depth}_1core.txt
 
     # Modify spdk.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" spdk.fio
-    sudo fio spdk.fio --output=output_spdk_${depth}_2core.txt
+    sudo fio spdk.fio --output-format=json -o $RESULTS/output_spdk_${depth}_1core.txt
 done
 
 echo "Tests completed."
