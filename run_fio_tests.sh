2d86519cad7b30b8c9146bcf7a350d82d09e8e3a
Abhinav <gabhinav001@gmail.com>
2024-06-05 20:40:22 +0000

    fix for spdk

--- run_fio_tests.sh
+++ run_fio_tests.sh
@@ -11,27 +11,36 @@ fi
 # List of iodepth values
 iodepths=(1 2 4 8 16 32 64 128)
 
+# reset hugepages
+sudo /home/gabhinav001/spdk/scripts/setup.sh reset
+
 # Loop through each iodepth value
 for depth in "${iodepths[@]}"; do
     # Modify iou.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" iou.fio
-    sudo fio iou.fio --output-format=json -o $RESULTS/output_iou_${depth}_1core.txt
+    sudo fio iou.fio --output-format=json -o $RESULTS/output_iou_${depth}_2core.txt
     
     # Modify iou_p.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" iou_p.fio
-    sudo fio iou_p.fio --output-format=json -o $RESULTS/output_iou_p_${depth}_1core.txt
+    sudo fio iou_p.fio --output-format=json -o $RESULTS/output_iou_p_${depth}_2core.txt
     
     # Modify iou_k.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" iou_k.fio
-    sudo fio iou_k.fio --output-format=json -o $RESULTS/output_iou_k_${depth}_1core.txt
+    sudo fio iou_k.fio --output-format=json -o $RESULTS/output_iou_k_${depth}_2core.txt
 
     # Modify aio.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" aio.fio
-    sudo fio aio.fio --output-format=json -o $RESULTS/output_aio_${depth}_1core.txt
+    sudo fio aio.fio --output-format=json -o $RESULTS/output_aio_${depth}_2core.txt
+done
 
+# set hugepages
+sudo HUGENODE="nodes_hp[0]=4096" /home/gabhinav001/spdk/scripts/setup.sh
+
+# Loop through each iodepth value
+for depth in "${iodepths[@]}"; do
     # Modify spdk.fio
     sed -i "s/iodepth=.*/iodepth=$depth/" spdk.fio
-    sudo LD_PRELOAD=/home/gabhinav001/spdk/build/fio/spdk_nvme fio spdk.fio --output-format=json -o $RESULTS/output_spdk_${depth}_1core.txt
+    sudo LD_PRELOAD=/home/gabhinav001/spdk/build/fio/spdk_nvme fio spdk.fio --output-format=json -o $RESULTS/output_spdk_${depth}_2core.txt
 done
 
 echo "Tests completed."
